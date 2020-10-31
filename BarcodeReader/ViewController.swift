8ed1bd5e36f27a585d71b10351db30ad92679cf1
miserii <miseri.osk12@gmail.com>
2020-10-31 14:43:27 +0900

    buttonを押した時の画面遷移を追加

--- BarcodeReader/ViewController.swift
+++ BarcodeReader/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func tapedCamera(_ sender: UIButton) {
+        self.present(BarCodeReaderViewController(), animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
