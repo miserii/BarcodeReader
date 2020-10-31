0ad1a61f82c7938287d4557d64c15ac241985a74
miserii <miseri.osk12@gmail.com>
2020-10-31 15:04:38 +0900

    コメント削除

--- BarcodeReader/ViewController.swift
+++ BarcodeReader/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
         tableView.delegate = self
         tableView.dataSource = self
     }
