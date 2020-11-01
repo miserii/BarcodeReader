b42b1d22bfdf1f32ac4d85a1bc7747e10c4f14fc
miserii <miseri.osk12@gmail.com>
2020-11-01 23:48:17 +0900

    Create New File

---
+++ BarcodeReader/SettingsTableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsTableViewController.swift
+//  BarcodeReader
+//
+//  Created by osakamiseri on 2020/11/01.
+//
+
+import UIKit
+
+class SettingsTableViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
