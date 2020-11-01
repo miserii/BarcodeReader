764ab6778e3f67cbfeb9d3157a28bc2a7f00a0ec
miserii <miseri.osk12@gmail.com>
2020-11-02 00:19:04 +0900

    メモ

--- BarcodeReader/ViewControllers/SettingsTableViewController.swift
+++ BarcodeReader/ViewControllers/SettingsTableViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class SettingsTableViewController: UITableViewController {
     
+//    設定項目 1.バージョン
+//    2.利用規約 3.ぷライバシーポリシー 4.ライセンス
+//    5.シェア 6.レビュー 7.マルシー
     
     override func viewDidLoad() {
         super.viewDidLoad()
